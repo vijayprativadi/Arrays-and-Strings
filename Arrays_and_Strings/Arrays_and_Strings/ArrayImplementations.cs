510c34b42a4f016d72fe31b0261268c2be7d3048
Vijay Prativadi <vijayprativadi@hotmail.com>
2020-09-18 23:03:30 -0400

    Added ValidateSequence & Monotonic Array

--- Arrays_and_Strings/Arrays_and_Strings/ArrayImplementations.cs
+++ Arrays_and_Strings/Arrays_and_Strings/ArrayImplementations.cs
@@ -26,5 +26,61 @@ namespace Arrays_and_Strings
 
             Console.WriteLine("Pair not found");
         }
+
+        public static bool ValidateSequence(int[] arr, int[] seq)
+        {
+            int arrIdx = 0;
+            int seqIdx = 0;
+            while (arrIdx < arr.Length - 1 && seqIdx < arr.Length -1)
+            {
+                if (arr[arrIdx] == seq[seqIdx])
+                {
+                    seqIdx++;
+                }
+                arrIdx++;
+            }
+            return seqIdx == seq.Length;
+        }
+
+        public static bool MonotonicArray(int[] arr)
+        {
+            bool isMonotonic = true;
+            string order = string.Empty;
+            if (arr.Length > 0)
+            {
+                if (arr[0] > arr[1])
+                {
+                    order = "DESC";
+                }
+                else
+                {
+                    order = "ASC";
+                }
+
+                for (int i = 1; i < arr.Length - 1; i++)
+                {
+                    if (order == "ASC")
+                    {
+                        if (arr[i + 1] < arr[i])
+                        {
+                            isMonotonic = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (arr[i + 1] > arr[i])
+                        {
+                            isMonotonic = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return isMonotonic;
+        }
+
     }
 }
+
--- Arrays_and_Strings/Arrays_and_Strings/Program.cs
+++ Arrays_and_Strings/Arrays_and_Strings/Program.cs
@@ -11,9 +11,15 @@ namespace Arrays_and_Strings
         static void Main(string[] args)
         {
             int[] arr = { 8, 7, 2, 5, 3, 1 };
+            int[] seq = { 7, 5, 1};
             int sum = 10;
 
             ArrayImplementations.FindPairForGivenSumArray(arr, sum);
+
+            Console.WriteLine("Is Sequence Valid: " + ArrayImplementations.ValidateSequence(arr, seq));
+
+            Console.WriteLine("Is Monotonic Array: " + ArrayImplementations.MonotonicArray(new int[] { 1, 2, 3, 0}));
+
 
             Console.ReadKey();
         }
